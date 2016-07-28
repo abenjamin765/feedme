48e58c52813426c2eeebe828bdd49529347ca7fd
seven11nash <momueller@me.com>
2016-07-28 13:39:26 +0200

    help button works, new feed works, etc

--- js/feedme.js
+++ js/feedme.js
@@ -6,6 +6,7 @@ var feedName = "Feed";
 var itemAmount = 2;
 var itemArray = [];
 var valueArray = ["0", "1"];
+var helpToggle = false;
 
 // CODE GEN FOR LINK
 function generateUniqueURL(){
@@ -108,6 +109,33 @@ $('#itemNumber').focusout('input',function(e){
 
 });
 
+// RESET FEED
+function newFeed(){
+	feedName = "Feed";
+	itemAmount = 2;
+	itemArray = [];
+	valueArray = ["0", "1"];
+	helpToggle = false;
+
+	$("#itemNumber").val(itemAmount);
+	$("#feedName").val(feedName);
+
+	generateUniqueURL();
+	updateFeed();
+}
+
+// DISPLAY HELP
+function toggleHelp(){
+	if(helpToggle == false){
+		helpToggle = true;
+		var helpText = "<b>Welcome to Feed.Me help</b><br><span>Note: To go back to your feed press the help button again</span><br><br><b>What is Feed.Me for?</b><br><span>Feed.Me is a tool for developers and designers, you can simply and easily generate spoof JSON data, which is hosted on Feed.Me or you can download as a json file. This helps prototyping and testing if your code would work with real JSON data.</span>";
+		document.getElementById("codeField").innerHTML = helpText;
+	}else{
+		helpToggle = false;
+		updateFeed();
+	}
+}
+
 // FUNCTION FOR THE COPY BUTTON
 function copyLink(){
 	var copyUrl = $("#path--field").val();
@@ -126,26 +154,28 @@ function copyLink(){
 
 // FUNCTION CONTROLLING WHAT IS SEEN IN THE MAIN STAGE (CODEFIELD)
 function updateFeed(){
+	if(helpToggle == false){
 
-	var finalJSON = "";
+		var finalJSON = "";
 
-	var beginning = '{<br>&nbsp;&nbsp;"' + feedName + '": {<br>';
-	finalJSON = beginning;
+		var beginning = '{<br>&nbsp;&nbsp;"' + feedName + '": {<br>';
+		finalJSON = beginning;
 
-	valueAmount = valueArray.length;
-	for(var i = 0; i < valueArray.length; i+=1){
+		valueAmount = valueArray.length;
+		for(var i = 0; i < valueArray.length; i+=1){
 
-		finalJSON = finalJSON + '&nbsp;&nbsp;&nbsp;&nbsp;"' + i + '": {<br><br>&nbsp;&nbsp;&nbsp;&nbsp;}<br>';
+			finalJSON = finalJSON + '&nbsp;&nbsp;&nbsp;&nbsp;"' + i + '": {<br><br>&nbsp;&nbsp;&nbsp;&nbsp;}<br>';
+
+		}
+		if(valueAmount == 0){
+			finalJSON = finalJSON + '<br>';
+		}
+		var end = "&nbsp;&nbsp;}<br>}";
+		finalJSON = finalJSON + end;
+
+		document.getElementById("codeField").innerHTML = finalJSON;
 
 	}
-	if(valueAmount == 0){
-		finalJSON = finalJSON + '<br>';
-	}
-	var end = "&nbsp;&nbsp;}<br>}";
-	finalJSON = finalJSON + end;
-
-	document.getElementById("codeField").innerHTML = finalJSON;
-
 }
 
 // INITIAL START
